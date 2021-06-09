8cefe8bf6d1bcb52dba3b96cf862b730fb4823ea
Sehyun <73203982+SHC02@users.noreply.github.com>
2021-06-09 22:51:37 +0000

    Add files via upload

---
+++ Java Programming Masterclass for Software Developers/Class, Constructors, and Inheritance/Main.java
@@ -0,0 +1,12 @@
+package classesePractice;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		// objects
+		Car porsche = new Car();
+		Car holden = new Car();
+		porsche.setModel("commodore");
+		System.out.println("Model is: " + porsche.getModel());
+	}
+}
---
+++ Java Programming Masterclass for Software Developers/Class, Constructors, and Inheritance/SimpleCalculator.java
@@ -0,0 +1,49 @@
+package simpleCalculator;
+
+public class SimpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+    
+    public double getFirstNumber()
+    {
+        return this.firstNumber;
+    }
+    public double getSecondNumber()
+    {
+        return this.secondNumber;
+    }
+    public void setFirstNumber(double firstNumber)
+    {
+        this.firstNumber = firstNumber;
+    }
+    public void setSecondNumber(double secondNumber)
+    {
+        this.secondNumber = secondNumber;
+    }
+    public double getAdditionResult()
+    {
+        return firstNumber + secondNumber;
+    }
+    public double getSubtractionResult()
+    {
+        return firstNumber - secondNumber;
+    }
+    public double getMultiplicationResult()
+    {
+        return firstNumber * secondNumber;
+    }
+    public double getDivisionResult()
+    {
+    	double result = 0.0;
+    	
+    	if(secondNumber == 0)
+    	{
+    		result = 0.0;
+    	}
+    	else
+    	{
+    		result = firstNumber / secondNumber;
+    	}
+        return result;
+    }
+}
